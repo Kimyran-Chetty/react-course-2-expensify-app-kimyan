b400860a17780f7663242404441c27ad6bd11fef
Kimyran Chetty <Kimyran.Chetty@standardbank.co.za>
2022-08-22 13:55:03 +0200

    Corrected expenseSummary test cases

--- src/tests/components/ExpenseSummary.test.js
+++ src/tests/components/ExpenseSummary.test.js
@@ -3,12 +3,12 @@ import {shallow} from 'enzyme';
 import {ExpenseSummary} from '../../components/ExpenseSummary';
 
 
-test('should render ExpenseForm', () => {
+test('should render ExpenseForm with one expense', () => {
     const wrapper = shallow(<ExpenseSummary expensesCount={1}  expensesTotal={235}/>);
     expect(wrapper).toMatchSnapshot();
 })
 
-test('should render ExpenseForm', () => {
-    const wrapper = shallow(<ExpenseSummary expensesVisible={23}  expensesTotal={245678345}/>);
+test('should render ExpenseForm with two expenses', () => {
+    const wrapper = shallow(<ExpenseSummary expensesCount={23}  expensesTotal={245678345}/>);
     expect(wrapper).toMatchSnapshot();
 })
